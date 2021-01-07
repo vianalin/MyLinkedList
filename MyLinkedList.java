dbde709d280bd984c8fd113a7e8d158c6540c746
vianalin <vlin21@stuy.edu>
2021-01-06 23:07:47 -0500

    constructor and size

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,17 +1,29 @@
 public class MyLinkedList{
 
 	private int size;
-	private Node start,end;  
+	private Node start, end;  
 
- 	public MyLinkedList(){/*create a constructor*/
-
+ 	public MyLinkedList(){
+ 		size = 0;
+ 		start = null;
+ 		end = null;
  	}
  	
- 	public int size();
+ 	public int size() {
+ 		return size;
+ 	}
+
  	public boolean add(String value);
+
  	public boolean add(int index, String value);
- 	public String get(int index);
+
+ 	public String get(int index) {
+ 		if(index < 0 || index >= size) throw new IndexOutOfBoundsException();
+ 	}
+
  	public String set(int index, String value);
+
  	public String toString();
+
  	//Any helper method that returns a Node object MUST BE PRIVATE!
 }
