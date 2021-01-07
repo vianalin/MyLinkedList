3a840f1dd3c275778566b6d3f053282669fe2fea
vianalin <vlin21@stuy.edu>
2021-01-07 00:53:11 -0500

    tested creation of mylinkedlist and size

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -3,27 +3,27 @@ public class MyLinkedList{
 	private int size;
 	private Node start, end;  
 
- 	public MyLinkedList(){
+ 	public MyLinkedList() {
  		size = 0;
- 		start = null;
- 		end = null;
  	}
  	
  	public int size() {
  		return size;
  	}
 
- 	public boolean add(String value);
+ 	//public boolean add(String value);
 
- 	public boolean add(int index, String value);
 
- 	public String get(int index) {
- 		if(index < 0 || index >= size) throw new IndexOutOfBoundsException();
- 	}
 
- 	public String set(int index, String value);
 
- 	public String toString();
+ 	//public boolean add(int index, String value);
+
+ 	//public String get(int index); 
+ 	//if(index < 0 || index >= size) throw new IndexOutOfBoundsException();
+
+ 	//public String set(int index, String value);
+
+ 	//public String toString();
 
  	//Any helper method that returns a Node object MUST BE PRIVATE!
 }
